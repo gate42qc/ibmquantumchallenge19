54ded543557fc3f752a2155a88c009b848c2ad2f
Misha Aghamalyan <misha.aghamalyan@gmail.com>
2019-10-14 02:50:21 +0400

    Add job submission utils

--- konbini_distribution/utils.py
+++ konbini_distribution/utils.py
@@ -117,3 +117,63 @@ def get_job_result(job_id: str):
     return job.result()
 
 
+def generate_contest_submission_file(qc, results):
+    # Input your quantum circuit
+    circuit = qc
+    # Input your result of the execute(groverCircuit, backend=backend, shots=shots).result()
+    results = results
+    count = results.get_counts()
+    # Provide your team name
+    name = 'Gate42'
+    # Please indicate the number of times you have made a submission so far.
+    # For example, if it's your 1st time to submit your answer, write 1. If it's your 5th time to submit your answer, write 5.
+    times = '3'
+
+    import json
+    from qiskit.transpiler import PassManager
+    from qiskit.transpiler.passes import Unroller
+
+    # Unroll the circuit
+    pass_ = Unroller(['u3', 'cx'])
+    pm = PassManager(pass_)
+    new_circuit = pm.run(circuit)
+
+    # obtain gates
+    gates = new_circuit.count_ops()
+
+    # sort count
+    count_sorted = sorted(count.items(), key=lambda x: x[1], reverse=True)
+
+    # collect answers with Top 9 probability
+    ans_list = count_sorted[0:9]
+
+    # reverse ans_list
+    ans_reversed = []
+    for i in ans_list:
+        ans_temp = [i[0][::-1], i[1]]
+        ans_reversed.append(ans_temp)
+
+    # convert each 2 bits into corresponding color. Add node0(0),node3(1),node8(2) and node11(3)
+    ans_shaped = []
+    for j in ans_reversed:
+        ans_temp = j[0]
+        nodeA = 0
+        node0 = int(ans_temp[0] + ans_temp[1], 2)
+        node1 = int(ans_temp[2] + ans_temp[3], 2)
+        nodeB = 1
+        node2 = int(ans_temp[4] + ans_temp[5], 2)
+        node3 = int(ans_temp[6] + ans_temp[7], 2)
+        node4 = int(ans_temp[8] + ans_temp[9], 2)
+        nodeC = 2
+        node5 = int(ans_temp[10] + ans_temp[11], 2)
+        node6 = int(ans_temp[12] + ans_temp[13], 2)
+        nodeD = 3
+        nodes_color = str(nodeA) + str(node0) + str(node1) + str(nodeB) + str(node2) + str(node3) + str(node4) + str(
+            nodeC) + str(node5) + str(node6) + str(nodeD)
+        ans_shaped.append([nodes_color, j[1]])
+
+    # write the result into '[your name]_final_output.txt'
+    filename = name + '_' + times + '_final_output.txt'
+    dct = {'ans': ans_shaped, 'costs': gates}
+    with open(filename, 'w') as f:
+        json.dump(dct, f)
